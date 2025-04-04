3c7219230bd370a8ffd0309600c762ecfc120ddc
itay mijan <itaym99@gmail.com>
2025-04-04 11:42:25 +0300

    stractured the piece.h file

--- Chess/include/Pieces/Piece.h
+++ Chess/include/Pieces/Piece.h
@@ -11,11 +11,12 @@ public:
     Piece(bool isWhite, int x, int y);
     virtual ~Piece() = default;
 
-    // Check if a move is valid for this piece
-    virtual bool isValidMove(int destX, int destY, const std::vector<std::vector<std::shared_ptr<Piece>>>& board) const = 0;
+    
+    virtual bool isValidMove(int destX, int destY, 
+        const std::vector<std::vector<std::shared_ptr<Piece>>>& board) const = 0; 
 
-    // Check if the path to the destination is clear (for pieces like rook, bishop, queen)
-    virtual bool isPathClear(int destX, int destY, const std::vector<std::vector<std::shared_ptr<Piece>>>& board) const = 0;
+    virtual bool isPathClear(int destX, int destY, 
+        const std::vector<std::vector<std::shared_ptr<Piece>>>& board) const = 0;
 
     // Getters
     bool getIsWhite() const;
@@ -25,7 +26,6 @@ public:
     // Setters
     void setPosition(int newX, int newY);
 
-    // Get the character representation of the piece
     virtual char getSymbol() const = 0;
 
 private:
