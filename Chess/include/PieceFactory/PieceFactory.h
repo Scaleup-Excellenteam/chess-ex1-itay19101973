cb23932f70aa9e9cf66f71277288088b03fa15a6
itay <itaym99@gmail.com>
2025-04-09 17:37:16 +0300

    added factory doc

--- Chess/include/PieceFactory/PieceFactory.h
+++ Chess/include/PieceFactory/PieceFactory.h
@@ -6,6 +6,15 @@
 #include <map>
 #include <memory>
 
+
+/**
+ * @class PieceFactory
+ *
+ * This class implements the Factory design pattern to allow creation of different
+ * chess pieces based on a character symbol. It supports runtime registration of
+ * piece types and uses function pointers or lambdas to instantiate them.
+ */
+
 class PieceFactory {
 public:
     using Creator = std::function<std::shared_ptr<Piece>(bool, int, int)>;
--- Chess/src/PieceFactory/PieceFactory.cpp
+++ Chess/src/PieceFactory/PieceFactory.cpp
@@ -1,11 +1,13 @@
 #include "PieceFactory/PieceFactory.h"
 
+//======================================================================
+// returns the factory map
 std::map<char, PieceFactory::Creator>& PieceFactory::getCreators() {
     static std::map<char, Creator> creators; // static pieces map factory
     return creators;
 }
 
-
+//======================================================================
 // adding a piece to the factory
 bool PieceFactory::registerPiece(char symbol, Creator creator) {
     char upperSymbol = std::toupper(symbol);
@@ -17,7 +19,7 @@ bool PieceFactory::registerPiece(char symbol, Creator creator) {
     return true;
 }
 
-
+//======================================================================
 // create the  piece
 std::shared_ptr<Piece> PieceFactory::createPiece(char symbol, int x, int y) {
     bool isWhite = std::isupper(symbol);
